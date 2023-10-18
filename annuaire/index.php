61ca87faf50e8bd3e26821f2c11fc8794df702ba
djedou <djedouarnaud.aman@gmail.com>
2023-10-19 00:25:01 +0200

    fix reloading page

--- annuaire/index.php
+++ annuaire/index.php
@@ -666,7 +666,7 @@
 		<form id="formSearchAnnuaire">
 			<div id="barre-recherche">
 				<input type="search" name="search" id="searchName" autocomplete="off" placeholder="mots-clés : ex informaticien" />
-				<button onclick="performSearch()" type="submit" class="recherche">
+				<button onclick="performSearch()" type="button" class="recherche">
 					<svg id="loupe" focusable="false" viewBox="0 0 24 24">
 						<path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
 					</svg>
@@ -816,7 +816,7 @@
 		}
 
 		// Appeler performSearch lors du chargement de la page pour afficher tous les éléments par défaut
-		window.onload = performSearch;
+		//window.onload = performSearch;
 
 		function displayForm(user) {
 			var contenairForm = document.getElementById('contenair-formulaire');
